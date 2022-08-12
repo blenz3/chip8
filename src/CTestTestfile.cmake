4802b8cd693829c7bb8c6357d2d21e72960a1a51
Braden Lenz <16659708+blenz3@users.noreply.github.com>
2022-08-12 16:02:57 -0700

    Cleanup

--- src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/bradenlenz/Documents/GitHub/chip8/src
-# Build directory: /Users/bradenlenz/Documents/GitHub/chip8/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
